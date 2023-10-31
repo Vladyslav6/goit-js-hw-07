92fb59fa3d2bd9fa3f2384041fe96a9b5b13e405
Vladyslav6 <vlad51284@gmail.com>
2023-10-31 19:52:51 +0200

    ds

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulGallery = document.querySelector(".gallery");
+
+const forMap = galleryItems.map(
+  (ImgItems) => `<li class="gallery__item">
+        <a class="gallery__link"  href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${ImgItems.preview}"
+            data-source="${ImgItems.original}"
+            alt="${ImgItems.description}"
+          />
+        </a>
+      </li>`
+);
+
+ulGallery.insertAdjacentHTML("beforeend", forMap.join(""));
+
+ulGallery.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  const { target } = event;
+
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }
+}
